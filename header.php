f18b72a7f1812e42c5b5ea4573ee2f8d5ce1bdea
Amritpal Singh <30387931@students.atmc.edu.au>
2023-01-26 11:56:04 +1100

    Change Colour

--- header.php
+++ header.php
@@ -112,7 +112,7 @@ body {
         }
         #header {
   
-            background: #dba39a;  /* fallback for old browsers */
+            background: #000080;  /* fallback for old browsers */
             /*background: -webkit-linear-gradient(to right, #061161, #780206);  /* Chrome 10-25, Safari 5.1-6 */
             /*background: linear-gradient(to right, #061161, #780206); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
